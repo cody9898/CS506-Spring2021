d852ec4e3912c943bdbae61755bee94a252a30e9
Dakota Clark-Singh <cody@sigres.com>
2021-02-22 02:03:42 -0500

    completed read csv and some distance functions

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,22 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    
+    with open(csv_file_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            strings = line.strip('\n').split(",")
+            row = []
+            for s in strings:
+                try:
+                    v = int(s)
+                except ValueError:
+                    try:
+                        v = float(s)
+                    except ValueError:
+                        v = str(s).strip('\"')
+                row.append(v)
+            matrix.append(row)
+    
+    return matrix
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,16 +1,36 @@
 def euclidean_dist(x, y):
+    check_rules(x, y)
     res = 0
     for i in range(len(x)):
         res += (x[i] - y[i])**2
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    check_rules(x, y)
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    check_rules(x, y)
+    set_x = set(x)
+    set_y = set(y)
+
+    intersect_x_y = set_x & set_y
+    union_x_y = set_x | set_y
+    return 1 - len(intersect_x_y)/len(union_x_y)
+
+def dot_product(x, y):
+    return
 
 def cosine_sim(x, y):
+    check_rules(x, y)
     raise NotImplementedError()
 
 # Feel free to add more
+def check_rules(x, y):
+    if x == [] or y == []:
+        raise ValueError("lengths must not be zero")
+    if len(x) != len(y):
+        raise ValueError("lengths must be equal")
